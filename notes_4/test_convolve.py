0c29cd022ea22d482a9d0a65f161173801b6c2ad
canizalesjaime <canizales.jaime16@gmail.com>
2026-03-07 16:46:46 -0500

    added python code to see convolution

--- notes_4/test_convolve.py
+++ notes_4/test_convolve.py
@@ -1,5 +1,33 @@
 import numpy as np
-f = [1,2,3]
-g = [1,2]
-print(np.convolve(f,g))
+from scipy.signal import convolve2d
 
+# f = [1,2,3,4]
+# g = [1,2]
+#print(np.convolve(f,g))
+
+i= [[0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,1,1,1,1,0,0],
+    [0,0,1,1,1,1,0,0],
+    [0,0,1,1,1,1,0,0],
+    [0,0,1,1,1,1,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0]]
+
+s_x= [[-1,0,1],
+      [-2,0,2],
+      [-1,0,1]]
+
+s_y= [[-1,-2,-1],
+      [0,0,0],
+      [1,2,1]]
+
+I_x=convolve2d(i, s_x, mode="same")
+I_y=convolve2d(i, s_y, mode="same")
+print(I_x)
+print()
+print(I_y)
+print()
+
+out = np.round(np.sqrt((I_x*I_x)+(I_y*I_y)),decimals=2)
+print(out)
